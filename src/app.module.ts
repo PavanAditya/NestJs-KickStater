8ef578ebf8443f7295e631cd66d20147181b4062
Pavan Aditya M S <msp.aditya99@gmail.com>
2021-04-20 00:20:52 +0530

    middlewasre file added, module updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AppMiddleware } from './app.middleware';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AppMiddleware)
+      .forRoutes({ path: 'cats', method: RequestMethod.GET });
+  }
+}
